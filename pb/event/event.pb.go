fa2c4c51443d1d44f1bb6a3db0744bfc41ecaf87
ssyshiyue <10524690+ssyshiyue@user.noreply.gitee.com>
2024-11-07 23:05:12 +0800

    [feature] 1.update proto defines

--- pb/event/event.pb.go
+++ pb/event/event.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.34.2
 // 	protoc        v5.27.0
-// source: proto/event.proto
+// source: proto/event/event.proto
 
 package event
 
@@ -19,16 +19,17 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-var File_proto_event_proto protoreflect.FileDescriptor
+var File_proto_event_event_proto protoreflect.FileDescriptor
 
-var file_proto_event_proto_rawDesc = []byte{
-	0x0a, 0x11, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x65, 0x76, 0x65, 0x6e, 0x74, 0x2e, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x12, 0x02, 0x70, 0x62, 0x42, 0x0c, 0x5a, 0x0a, 0x2e, 0x2f, 0x70, 0x62, 0x2f,
-	0x65, 0x76, 0x65, 0x6e, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_proto_event_event_proto_rawDesc = []byte{
+	0x0a, 0x17, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x65, 0x76, 0x65, 0x6e, 0x74, 0x2f, 0x65, 0x76,
+	0x65, 0x6e, 0x74, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x05, 0x65, 0x76, 0x65, 0x6e, 0x74,
+	0x42, 0x0e, 0x5a, 0x0c, 0x2e, 0x2f, 0x2e, 0x2f, 0x70, 0x62, 0x2f, 0x65, 0x76, 0x65, 0x6e, 0x74,
+	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
-var file_proto_event_proto_goTypes = []any{}
-var file_proto_event_proto_depIdxs = []int32{
+var file_proto_event_event_proto_goTypes = []any{}
+var file_proto_event_event_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -36,26 +37,26 @@ var file_proto_event_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_proto_event_proto_init() }
-func file_proto_event_proto_init() {
-	if File_proto_event_proto != nil {
+func init() { file_proto_event_event_proto_init() }
+func file_proto_event_event_proto_init() {
+	if File_proto_event_event_proto != nil {
 		return
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_proto_event_proto_rawDesc,
+			RawDescriptor: file_proto_event_event_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   0,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_proto_event_proto_goTypes,
-		DependencyIndexes: file_proto_event_proto_depIdxs,
+		GoTypes:           file_proto_event_event_proto_goTypes,
+		DependencyIndexes: file_proto_event_event_proto_depIdxs,
 	}.Build()
-	File_proto_event_proto = out.File
-	file_proto_event_proto_rawDesc = nil
-	file_proto_event_proto_goTypes = nil
-	file_proto_event_proto_depIdxs = nil
+	File_proto_event_event_proto = out.File
+	file_proto_event_event_proto_rawDesc = nil
+	file_proto_event_event_proto_goTypes = nil
+	file_proto_event_event_proto_depIdxs = nil
 }

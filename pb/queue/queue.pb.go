ddd572e86c33451621c5effb09b7fd9c1c9bff6b
ssyshiyue <10524690+ssyshiyue@user.noreply.gitee.com>
2024-11-07 16:35:52 +0800

    [feature] 1.update proto defines

--- pb/queue/queue.pb.go
+++ pb/queue/queue.pb.go
@@ -20,98 +20,6 @@ const (
 	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
 )
 
-type SessionEvent int32
-
-const (
-	SessionEvent_OPEN   SessionEvent = 0
-	SessionEvent_CLOSED SessionEvent = 1
-)
-
-// Enum value maps for SessionEvent.
-var (
-	SessionEvent_name = map[int32]string{
-		0: "OPEN",
-		1: "CLOSED",
-	}
-	SessionEvent_value = map[string]int32{
-		"OPEN":   0,
-		"CLOSED": 1,
-	}
-)
-
-func (x SessionEvent) Enum() *SessionEvent {
-	p := new(SessionEvent)
-	*p = x
-	return p
-}
-
-func (x SessionEvent) String() string {
-	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
-}
-
-func (SessionEvent) Descriptor() protoreflect.EnumDescriptor {
-	return file_proto_queue_proto_enumTypes[0].Descriptor()
-}
-
-func (SessionEvent) Type() protoreflect.EnumType {
-	return &file_proto_queue_proto_enumTypes[0]
-}
-
-func (x SessionEvent) Number() protoreflect.EnumNumber {
-	return protoreflect.EnumNumber(x)
-}
-
-// Deprecated: Use SessionEvent.Descriptor instead.
-func (SessionEvent) EnumDescriptor() ([]byte, []int) {
-	return file_proto_queue_proto_rawDescGZIP(), []int{0}
-}
-
-type SessionType int32
-
-const (
-	SessionType_SYNC  SessionType = 0
-	SessionType_ASYNC SessionType = 1
-)
-
-// Enum value maps for SessionType.
-var (
-	SessionType_name = map[int32]string{
-		0: "SYNC",
-		1: "ASYNC",
-	}
-	SessionType_value = map[string]int32{
-		"SYNC":  0,
-		"ASYNC": 1,
-	}
-)
-
-func (x SessionType) Enum() *SessionType {
-	p := new(SessionType)
-	*p = x
-	return p
-}
-
-func (x SessionType) String() string {
-	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
-}
-
-func (SessionType) Descriptor() protoreflect.EnumDescriptor {
-	return file_proto_queue_proto_enumTypes[1].Descriptor()
-}
-
-func (SessionType) Type() protoreflect.EnumType {
-	return &file_proto_queue_proto_enumTypes[1]
-}
-
-func (x SessionType) Number() protoreflect.EnumNumber {
-	return protoreflect.EnumNumber(x)
-}
-
-// Deprecated: Use SessionType.Descriptor instead.
-func (SessionType) EnumDescriptor() ([]byte, []int) {
-	return file_proto_queue_proto_rawDescGZIP(), []int{1}
-}
-
 type QueueMsg struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -836,61 +744,6 @@ func (x *ProvisionResponse) GetCredentials() *DeviceCredentialsProto {
 	return nil
 }
 
-type DeviceSessionEventNotify struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Type  SessionType  `protobuf:"varint,1,opt,name=type,proto3,enum=pb.SessionType" json:"type,omitempty"`
-	Event SessionEvent `protobuf:"varint,2,opt,name=event,proto3,enum=pb.SessionEvent" json:"event,omitempty"`
-}
-
-func (x *DeviceSessionEventNotify) Reset() {
-	*x = DeviceSessionEventNotify{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_proto_queue_proto_msgTypes[11]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *DeviceSessionEventNotify) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*DeviceSessionEventNotify) ProtoMessage() {}
-
-func (x *DeviceSessionEventNotify) ProtoReflect() protoreflect.Message {
-	mi := &file_proto_queue_proto_msgTypes[11]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use DeviceSessionEventNotify.ProtoReflect.Descriptor instead.
-func (*DeviceSessionEventNotify) Descriptor() ([]byte, []int) {
-	return file_proto_queue_proto_rawDescGZIP(), []int{11}
-}
-
-func (x *DeviceSessionEventNotify) GetType() SessionType {
-	if x != nil {
-		return x.Type
-	}
-	return SessionType_SYNC
-}
-
-func (x *DeviceSessionEventNotify) GetEvent() SessionEvent {
-	if x != nil {
-		return x.Event
-	}
-	return SessionEvent_OPEN
-}
-
 var File_proto_queue_proto protoreflect.FileDescriptor
 
 var file_proto_queue_proto_rawDesc = []byte{
@@ -989,20 +842,9 @@ var file_proto_queue_proto_rawDesc = []byte{
 	0x6f, 0x12, 0x3c, 0x0a, 0x0b, 0x63, 0x72, 0x65, 0x64, 0x65, 0x6e, 0x74, 0x69, 0x61, 0x6c, 0x73,
 	0x18, 0x03, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x1a, 0x2e, 0x70, 0x62, 0x2e, 0x44, 0x65, 0x76, 0x69,
 	0x63, 0x65, 0x43, 0x72, 0x65, 0x64, 0x65, 0x6e, 0x74, 0x69, 0x61, 0x6c, 0x73, 0x50, 0x72, 0x6f,
-	0x74, 0x6f, 0x52, 0x0b, 0x63, 0x72, 0x65, 0x64, 0x65, 0x6e, 0x74, 0x69, 0x61, 0x6c, 0x73, 0x22,
-	0x67, 0x0a, 0x18, 0x44, 0x65, 0x76, 0x69, 0x63, 0x65, 0x53, 0x65, 0x73, 0x73, 0x69, 0x6f, 0x6e,
-	0x45, 0x76, 0x65, 0x6e, 0x74, 0x4e, 0x6f, 0x74, 0x69, 0x66, 0x79, 0x12, 0x23, 0x0a, 0x04, 0x74,
-	0x79, 0x70, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0e, 0x32, 0x0f, 0x2e, 0x70, 0x62, 0x2e, 0x53,
-	0x65, 0x73, 0x73, 0x69, 0x6f, 0x6e, 0x54, 0x79, 0x70, 0x65, 0x52, 0x04, 0x74, 0x79, 0x70, 0x65,
-	0x12, 0x26, 0x0a, 0x05, 0x65, 0x76, 0x65, 0x6e, 0x74, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0e, 0x32,
-	0x10, 0x2e, 0x70, 0x62, 0x2e, 0x53, 0x65, 0x73, 0x73, 0x69, 0x6f, 0x6e, 0x45, 0x76, 0x65, 0x6e,
-	0x74, 0x52, 0x05, 0x65, 0x76, 0x65, 0x6e, 0x74, 0x2a, 0x24, 0x0a, 0x0c, 0x53, 0x65, 0x73, 0x73,
-	0x69, 0x6f, 0x6e, 0x45, 0x76, 0x65, 0x6e, 0x74, 0x12, 0x08, 0x0a, 0x04, 0x4f, 0x50, 0x45, 0x4e,
-	0x10, 0x00, 0x12, 0x0a, 0x0a, 0x06, 0x43, 0x4c, 0x4f, 0x53, 0x45, 0x44, 0x10, 0x01, 0x2a, 0x22,
-	0x0a, 0x0b, 0x53, 0x65, 0x73, 0x73, 0x69, 0x6f, 0x6e, 0x54, 0x79, 0x70, 0x65, 0x12, 0x08, 0x0a,
-	0x04, 0x53, 0x59, 0x4e, 0x43, 0x10, 0x00, 0x12, 0x09, 0x0a, 0x05, 0x41, 0x53, 0x59, 0x4e, 0x43,
-	0x10, 0x01, 0x42, 0x0c, 0x5a, 0x0a, 0x2e, 0x2f, 0x70, 0x62, 0x2f, 0x71, 0x75, 0x65, 0x75, 0x65,
-	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x74, 0x6f, 0x52, 0x0b, 0x63, 0x72, 0x65, 0x64, 0x65, 0x6e, 0x74, 0x69, 0x61, 0x6c, 0x73, 0x42,
+	0x0c, 0x5a, 0x0a, 0x2e, 0x2f, 0x70, 0x62, 0x2f, 0x71, 0x75, 0x65, 0x75, 0x65, 0x62, 0x06, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -1017,41 +859,35 @@ func file_proto_queue_proto_rawDescGZIP() []byte {
 	return file_proto_queue_proto_rawDescData
 }
 
-var file_proto_queue_proto_enumTypes = make([]protoimpl.EnumInfo, 2)
-var file_proto_queue_proto_msgTypes = make([]protoimpl.MessageInfo, 13)
+var file_proto_queue_proto_msgTypes = make([]protoimpl.MessageInfo, 12)
 var file_proto_queue_proto_goTypes = []any{
-	(SessionEvent)(0),                  // 0: pb.SessionEvent
-	(SessionType)(0),                   // 1: pb.SessionType
-	(*QueueMsg)(nil),                   // 2: pb.QueueMsg
-	(*CommonResponse)(nil),             // 3: pb.CommonResponse
-	(*ValidateDeviceAuthRequest)(nil),  // 4: pb.ValidateDeviceAuthRequest
-	(*ValidateDeviceAuthResponse)(nil), // 5: pb.ValidateDeviceAuthResponse
-	(*DeviceInfoProto)(nil),            // 6: pb.DeviceInfoProto
-	(*ProductInfoProto)(nil),           // 7: pb.ProductInfoProto
-	(*DeviceCredentialsProto)(nil),     // 8: pb.DeviceCredentialsProto
-	(*ProvisionConnectRequest)(nil),    // 9: pb.ProvisionConnectRequest
-	(*ProvisionConnectResponse)(nil),   // 10: pb.ProvisionConnectResponse
-	(*ProvisionRequest)(nil),           // 11: pb.ProvisionRequest
-	(*ProvisionResponse)(nil),          // 12: pb.ProvisionResponse
-	(*DeviceSessionEventNotify)(nil),   // 13: pb.DeviceSessionEventNotify
-	nil,                                // 14: pb.QueueMsg.HeadersEntry
+	(*QueueMsg)(nil),                   // 0: pb.QueueMsg
+	(*CommonResponse)(nil),             // 1: pb.CommonResponse
+	(*ValidateDeviceAuthRequest)(nil),  // 2: pb.ValidateDeviceAuthRequest
+	(*ValidateDeviceAuthResponse)(nil), // 3: pb.ValidateDeviceAuthResponse
+	(*DeviceInfoProto)(nil),            // 4: pb.DeviceInfoProto
+	(*ProductInfoProto)(nil),           // 5: pb.ProductInfoProto
+	(*DeviceCredentialsProto)(nil),     // 6: pb.DeviceCredentialsProto
+	(*ProvisionConnectRequest)(nil),    // 7: pb.ProvisionConnectRequest
+	(*ProvisionConnectResponse)(nil),   // 8: pb.ProvisionConnectResponse
+	(*ProvisionRequest)(nil),           // 9: pb.ProvisionRequest
+	(*ProvisionResponse)(nil),          // 10: pb.ProvisionResponse
+	nil,                                // 11: pb.QueueMsg.HeadersEntry
 }
 var file_proto_queue_proto_depIdxs = []int32{
-	14, // 0: pb.QueueMsg.headers:type_name -> pb.QueueMsg.HeadersEntry
-	3,  // 1: pb.ValidateDeviceAuthResponse.common:type_name -> pb.CommonResponse
-	6,  // 2: pb.ValidateDeviceAuthResponse.deviceInfo:type_name -> pb.DeviceInfoProto
-	3,  // 3: pb.ProvisionConnectResponse.common:type_name -> pb.CommonResponse
-	8,  // 4: pb.ProvisionRequest.credentials:type_name -> pb.DeviceCredentialsProto
-	3,  // 5: pb.ProvisionResponse.common:type_name -> pb.CommonResponse
-	6,  // 6: pb.ProvisionResponse.deviceInfo:type_name -> pb.DeviceInfoProto
-	8,  // 7: pb.ProvisionResponse.credentials:type_name -> pb.DeviceCredentialsProto
-	1,  // 8: pb.DeviceSessionEventNotify.type:type_name -> pb.SessionType
-	0,  // 9: pb.DeviceSessionEventNotify.event:type_name -> pb.SessionEvent
-	10, // [10:10] is the sub-list for method output_type
-	10, // [10:10] is the sub-list for method input_type
-	10, // [10:10] is the sub-list for extension type_name
-	10, // [10:10] is the sub-list for extension extendee
-	0,  // [0:10] is the sub-list for field type_name
+	11, // 0: pb.QueueMsg.headers:type_name -> pb.QueueMsg.HeadersEntry
+	1,  // 1: pb.ValidateDeviceAuthResponse.common:type_name -> pb.CommonResponse
+	4,  // 2: pb.ValidateDeviceAuthResponse.deviceInfo:type_name -> pb.DeviceInfoProto
+	1,  // 3: pb.ProvisionConnectResponse.common:type_name -> pb.CommonResponse
+	6,  // 4: pb.ProvisionRequest.credentials:type_name -> pb.DeviceCredentialsProto
+	1,  // 5: pb.ProvisionResponse.common:type_name -> pb.CommonResponse
+	4,  // 6: pb.ProvisionResponse.deviceInfo:type_name -> pb.DeviceInfoProto
+	6,  // 7: pb.ProvisionResponse.credentials:type_name -> pb.DeviceCredentialsProto
+	8,  // [8:8] is the sub-list for method output_type
+	8,  // [8:8] is the sub-list for method input_type
+	8,  // [8:8] is the sub-list for extension type_name
+	8,  // [8:8] is the sub-list for extension extendee
+	0,  // [0:8] is the sub-list for field type_name
 }
 
 func init() { file_proto_queue_proto_init() }
@@ -1192,32 +1028,19 @@ func file_proto_queue_proto_init() {
 				return nil
 			}
 		}
-		file_proto_queue_proto_msgTypes[11].Exporter = func(v any, i int) any {
-			switch v := v.(*DeviceSessionEventNotify); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_proto_queue_proto_rawDesc,
-			NumEnums:      2,
-			NumMessages:   13,
+			NumEnums:      0,
+			NumMessages:   12,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
 		GoTypes:           file_proto_queue_proto_goTypes,
 		DependencyIndexes: file_proto_queue_proto_depIdxs,
-		EnumInfos:         file_proto_queue_proto_enumTypes,
 		MessageInfos:      file_proto_queue_proto_msgTypes,
 	}.Build()
 	File_proto_queue_proto = out.File
